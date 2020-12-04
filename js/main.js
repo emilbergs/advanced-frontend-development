75a4bb8073d168230c3b60e7890ff30b59167484
Neibye <60384297+Neibye@users.noreply.github.com>
2020-12-04 11:05:36 +0100

    singe page application

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 const burger = document.querySelector(".burger");
+const white = document.querySelector(".white")
+const nav = document.querySelector(".navBar")
+const logo = document.querySelector(".logo")
+const main = document.querySelector(".main")
 
 burger.addEventListener("click", function(){
     burger.classList.toggle("open");
+    nav.classList.toggle("show")
+    logo.classList.toggle("hide")
+    main.classList.toggle("hide")
 });
+
+function show() {
+    burger.classList.toggle("open");
+    nav.classList.toggle("show")
+    logo.classList.toggle("hide")
+    main.classList.toggle("hide")
+}
