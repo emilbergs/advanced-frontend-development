c5f219862a70413f9bf4f4e1017bb0a4413062f4
Neibye <60384297+Neibye@users.noreply.github.com>
2020-12-04 13:13:26 +0100

    start på øl siden

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const emailRef = db.collection("email");
+const postRef = db.collection("posts");
 
 const burger = document.querySelector(".burger");
 const white = document.querySelector(".white")
@@ -40,4 +41,18 @@ function createEmail() {
     };
     emailRef.add(newEmail);
     emailInput.value = "Thanks!";
+}
+
+function appendPosts(demos) {
+    let htmlTemplate = "";
+    for (let post of posts) {
+        console.log(post.id);
+        console.log(post.artist);
+        htmlTemplate += `
+    <article>
+    <img src="${post.image}">
+    </article>
+    `;
+    }
+    document.querySelector('#demos').innerHTML = '<a href="#home"><img src="img/returnbutton2.png" class="backbutton" alt="backbutton"></a>' + "<h1>Public demos</h1>" + "<div class='flexContainer'>" + htmlTemplate + "</div>"
 }
