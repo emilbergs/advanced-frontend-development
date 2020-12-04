9ab676ed171ba4e0a522b4a9632f997b489e3458
Neibye <60384297+Neibye@users.noreply.github.com>
2020-12-04 12:51:38 +0100

    Forside done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDNik69yt4GrIvYjA5_GahGfIf2V1f6w94",
+    authDomain: "advanced-frontend-88240.firebaseapp.com",
+    projectId: "advanced-frontend-88240",
+    storageBucket: "advanced-frontend-88240.appspot.com",
+    messagingSenderId: "33450552290",
+    appId: "1:33450552290:web:021aa199be8b645e0ea7bb"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const emailRef = db.collection("email");
+
 const burger = document.querySelector(".burger");
 const white = document.querySelector(".white")
 const nav = document.querySelector(".navBar")
@@ -16,4 +30,14 @@ function show() {
     nav.classList.toggle("show")
     logo.classList.toggle("hide")
     main.classList.toggle("hide")
+}
+
+function createEmail() {
+    // references to the input fields
+    let emailInput = document.querySelector("#yourEmail");
+    let newEmail = {
+        email: emailInput.value,
+    };
+    emailRef.add(newEmail);
+    emailInput.value = "Thanks!";
 }
