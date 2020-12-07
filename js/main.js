a276fcd1d106f8e007bea5ed67b8fb15d77e7d66
Neibye <60384297+Neibye@users.noreply.github.com>
2020-12-07 14:49:53 +0100

    good luck

--- js/main.js
+++ js/main.js
@@ -52,6 +52,7 @@ postRef.onSnapshot(function (snapshotData) {
         posts.push(post);
     });
     appendPosts(posts);
+    appendPostsBygSelv(posts);
 });
 
 function appendPosts(posts) {
@@ -68,24 +69,72 @@ function appendPosts(posts) {
     document.querySelector('.content').innerHTML = htmlTemplate
 }
 
-function bygSelv() {
-    // references to the input fields
-    let kasse1Input = document.querySelector("#kasse1").value;
-    let kasse2Input = document.querySelector("#kasse2").value;
-    let kasse3Input = document.querySelector("#kasse3").value;
-    let kasse4Input = document.querySelector("#kasse4").value;
+let bygSelvArray = [];
 
-    let newUser = {
-        year: "2020",
-        yearCows: nrCowsInput.value,
-        kgMilk: kgMilkInput.value,
-        diesel: dieselInput.value,
-        electricity: electricityInput.value,
-        dryMatter: dryMatterInput.value,
-        selfFeed: selfFeedInput.value
-    };
-      firebase.auth().onAuthStateChanged(function (user) {
-      console.log(user.email);
-      year6.doc(user.email).set(newUser);
-    });
+function appendBygSelv(kasse){
+    bygSelvArray.push(kasse)
+    console.log(bygSelvArray)
+}
+function removeBygSelv() {
+    bygSelvArray.pop()
+    document.querySelectorAll(".checkbox").checked = false;
+    while(bygSelvOlArray.length > 0) {
+        bygSelvOlArray.pop();
+    }
+    let items=document.querySelectorAll('.checkbox');
+				for(let i=0; i<items.length; i++){
+					if(items[i].type=='checkbox')
+						items[i].checked=false;
+	}
+}
+
+
+
+function appendPostsBygSelv(posts) {
+    let htmlTemplate = "";
+    for (let post of posts) {
+        htmlTemplate += /*html*/ `
+        <article id="${post.id}">
+        <h3>${post.name}</h3>
+        <img src="${post.image}" class="postImage"><br>
+        <a id="${post.id}">Læs mere</a><br>
+        <form>
+            <label for="checked">Vælg:</label>
+            <input type="checkbox" name="checked" id="${post.id}" class="checkbox" onclick="appendBygSelvOl(this.id); limitArray()">
+        </form>
+        </article>
+    `;
+    }
+    document.querySelector('#bygSelvContent').innerHTML = htmlTemplate
+}
+
+let bygSelvOlArray = [];
+
+function appendBygSelvOl(ol) {
+    let checkBox = document.querySelector(".checkbox");
+    if (checkBox.checked == true){
+    bygSelvOlArray.push(ol)
+    console.log(bygSelvOlArray)
+    }
+}
+
+function limitArray() {
+    let kasse1 = 4;
+    let kasse2 = 8;
+    let kasse3 = 12;
+    let kasse4 = 16;
+
+    if (bygSelvArray.includes("kasse1") && kasse1 === bygSelvOlArray.length) {
+        console.log("Kasse 1 fyldt")
+        window.location = "#step3"
+    } else if (bygSelvArray.includes("kasse2") && kasse2 === bygSelvOlArray.length) {
+        console.log("Kasse 2 fyldt")
+        window.location = "#step3"
+    } else if (bygSelvArray.includes("kasse3") && kasse3 === bygSelvOlArray.length) {
+        console.log("Kasse 3 fyldt")
+        window.location = "#step3"
+    } else if (bygSelvArray.includes("kasse4") && kasse4 === bygSelvOlArray.length) {
+        console.log("Kasse 4 fyldt")
+        window.location = "#step3"
+    }
 }
